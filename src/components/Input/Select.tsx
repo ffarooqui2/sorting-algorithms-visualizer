fab9c568fe1294856aa9c2af3762b5049cbdd07e
Faaizuddin Farooqui <ffaro3@uic.edu>
2024-05-10 02:03:45 -0500

    updated background

--- src/components/Input/Select.tsx
+++ src/components/Input/Select.tsx
@@ -26,7 +26,7 @@ export const Select = ({
           </option>
         ))}
       </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 ">
         <svg
           className="fill-gray-300 h-4 w-4"
           xmlns="http://www.w3.org/2000/svg"
