af5555151f0776f19656d2c28a11a23639f07815
Faaizuddin Farooqui <ffaro3@uic.edu>
2024-05-10 02:12:25 -0500

    fixed bubble sort

--- src/algorithms/bubbleSort.ts
+++ src/algorithms/bubbleSort.ts
@@ -1,4 +1,4 @@
-import { AnimationArrayType } from "@/lib/utils";
+import { AnimationArrayType } from "@/lib/types";
 
 function runBubbleSort(array: number[], animations: AnimationArrayType) {
   for (let i = 0; i < array.length - 1; i++) {
